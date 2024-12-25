c6b3f4069d273d1010a3fdcf61b12cec39b9a5b9
tanzid64 <tanzid3@gmail.com>
2024-12-26 03:40:25 +0600

    feat: User manager & model created

---
+++ project_management/managers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import BaseUserManager
+from typing import Optional
+from django.db.models import Model
+
+
+class UserManager(BaseUserManager):
+    """
+    A custom user manager to deal with emails as unique identifiers for authentication instead of usernames.
+    """
+
+    def create_user(
+        self, email: str, password: Optional[str] = None, **extra_fields
+    ) -> Model:
+        """
+        Create and return a regular user with the given email and password.
+        """
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, email: str, password: Optional[str] = None, **extra_fields
+    ) -> Model:
+        """
+        Create and return a superuser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
--- project_management/models.py
+++ project_management/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from project_management.managers import UserManager
 
 # Create your models here.
+class TimeStampMixin(models.Model):
+    """
+    An abstract base class model that provides self-updating 'created_at' and 'updated_at' fields.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser):
+    """
+    Custom User model with email as the unique identifier.
+    """
+
+    email = models.EmailField(
+        verbose_name=_("Email Address"), unique=True, db_index=True
+    )
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",  # Avoid conflicts with default auth.User
+        blank=True,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        verbose_name=_("groups"),
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions_set",  # Avoid conflicts with default auth.User
+        blank=True,
+        help_text=_("Specific permissions for this user."),
+        verbose_name=_("user permissions"),
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        ordering = ["-date_joined"]  # Descending order
+
+    @property
+    def get_full_name(self) -> str:
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
